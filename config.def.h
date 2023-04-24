837a46e686174fa7be7b22142d2f05ecfdd137a0
Mouad Alami <moadalami40@gmail.com>
2023-04-24 15:22:06 +0100

    added bold is not bright patch and changed colors

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,47 +94,42 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+    /* 8 normal colors */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+    "#073642", /*  0: black    */
+    "#dc322f", /*  1: red      */
+    "#859900", /*  2: green    */
+    "#b58900", /*  3: yellow   */
+    "#268bd2", /*  4: blue     */
+    "#d33682", /*  5: magenta  */
+    "#2aa198", /*  6: cyan     */
+    "#eee8d5", /*  7: white    */
+    "#002b36", /*  8: brblack  */
+    "#cb4b16", /*  9: brred    */
+    "#586e75", /* 10: brgreen  */
+    "#657b83", /* 11: bryellow */
+    "#839496", /* 12: brblue   */
+    "#6c71c4", /* 13: brmagenta*/
+    "#93a1a1", /* 14: brcyan   */
+    "#fdf6e3", /* 15: brwhite  */
+                                   
+    /* special colors */
+    [256] = "#282a36", /* background */
+    [257] = "#f8f8f2", /* foreground */
+    [258] = "#00FF00"
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor, revresecolor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
